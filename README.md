0213376e7cd6f2db0b1390bbf3e2fc072607bd30
CHU SON <chuson1996@gmail.com>
2015-11-01 20:45:21 +0200

    Modulize

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+#Append file
+
+---
+
+A node module to easily insert content from a file to another. Check out the example below.
+
+---
+
+```
+<!-- dest.html -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,15 +15,13 @@
     <title></title>
     <style>
         /*<sou.css>*/
-        .add{
-            background-color:red;
-        }
-        .substract{
-            color:green;
-        }
         /*</sou.css>*/
     </style>
 </head>
 <body>
 </body>
 </html>
+```
+
+Now remember to always 
+ 
---
+++ bin/append.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+/**
+ * Created by chuso_000 on 1/11/2015.
+ */
+
+
+
+var fs = require('fs');
+var async = require('async');
+var path = require('path');
+
+
+var root = process.cwd();
+var destFile = path.resolve(root, process.argv[2]);
+fs.readFile(destFile, 'utf8', function (err, data) {
+    if (err) return console.error(err);
+    replaceContent(data);
+});
+
+
+function replaceContent(dest){
+    dest.match(/\/\*<([a-zA-Z0-9\._]+)>\*\/(.|[\r\n\t\s])+?\/\*<\/\1>\*\//g).forEach(function (pair) {
+        var start = pair.match(/\/\*<([a-zA-Z0-9\._]+)>\*\//)[0];
+        var end = pair.match(/\/\*<\/([a-zA-Z0-9\._]+)>\*\//)[0];
+        var file = path.resolve(root, pair.match(/[^\/\*<>]+/)[0]);
+        //console.log(dest.indexOf(start));
+        //console.log(dest.indexOf(end));
+        //console.log(file);
+
+        var indent = dest.match(/(\s+)(?=\/\*<([a-zA-Z0-9\._]+)>\*\/)/gm)[0].replace(/[\n\r]/g,'');
+        async.waterfall([
+            function findAndReadSourceFile(cb) {
+                fs.readFile(file, 'utf8', function (err, sou) {
+                    if (err) return cb(err);
+                    sou = sou.split('\n').map(function (line) {
+                        return indent+line;
+                    }).join('\n');
+
+                    cb(null, sou);
+                })
+            },
+            function writeContentToFile(sou, cb) {
+                //start+"\r\n"+sou
+                var newContent = (dest.replace(dest.substring(dest.indexOf(start), dest.indexOf(end)), start+"\r\n"+sou+"\r\n"+indent));
+                fs.writeFile(destFile, newContent, "utf8", function (err) {
+                    if (err) return cb(err);
+                });
+                cb(null);
+            }
+        ], function (err, result) {
+            if (err) return console.error(err);
+        })
+    });
+}
